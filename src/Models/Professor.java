12977b13dda42901b096f78792034560d63ebe85
Ramon Miguel <158791962+RamonMiguel717@users.noreply.github.com>
2024-11-24 00:24:29 -0300

    Add files via upload

--- src/Models/Professor.java
+++ src/Models/Professor.java
@@ -1,47 +1,50 @@
 package model;
 
 public class Professor {
-	private int idade;
 	private String nome;
+	private int idade;
 	private String especialidade;
 	private String cpf;
-	
-	public Professor(String nome,int idade, String especialidade,String cpf) {
-		
+
+	// Construtor
+	public Professor(String nome, int idade, String especialidade, String cpf) {
 		this.nome = nome;
 		this.idade = idade;
 		this.especialidade = especialidade;
 		this.cpf = cpf;
 	}
-	
-	public int getIdade() {
-		return idade;
-	}
+
+	// Getters
 	public String getNome() {
 		return nome;
 	}
+
+	public int getIdade() {
+		return idade;
+	}
+
 	public String getEspecialidade() {
 		return especialidade;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
-	
-	public void SetIdade(int idade) {
-		if (idade < 0) {
-			throw new IllegalArgumentException("Idade não pode ser negativa");
-		}if (idade < 18) {
-			throw new IllegalArgumentException("Professores não podem ser menores de idade");
-		}
-		this.idade = idade;
-	}
-	public void SetNome(String nome) {
+
+	// Setters
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public void SetEspecialidade(String especialidade) {
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public void setEspecialidade(String especialidade) {
 		this.especialidade = especialidade;
 	}
-	public void SetCPF(String cpf) {
+
+	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 }
